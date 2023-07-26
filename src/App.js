d06e21015681f373aaee1f41cdac102bdd39ace4
ceongroup <ceongroup@gmail.com>
2023-07-26 10:16:04 +0700

    add first file

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello world from ReactJS
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.dev"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Dev
         </a>
       </header>
     </div>

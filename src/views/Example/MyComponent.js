cda92620500b27822ea43d3834ccddf000d46716
ceongroup <ceongroup@gmail.com>
2023-07-27 16:51:36 +0700

    react component

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -2,9 +2,44 @@ import React from 'react';
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: "Leo",
+        channel: "Nhac Khong Loi"
+    }
+
+    handleClickButton = () => {
+        alert('Click Me')
+    }
+    handleOnChangeName = (event) => {
+        // console.log(event.target.value, event.target, event)
+        this.setState(
+            {
+                name: event.target.value
+            }
+        )
+    }
+
+
     render() {
+        let name = "Leo Nguyen";
+        // console.log(">>>> call render: ", this.state)
+
         return (
-            <div>Hello My Component </div>
+            // Fragment 
+            <>
+                <div className='firstdiv'>
+                    <input value={this.state.name} type="text" onChange={(event) => this.handleOnChangeName(event)} />
+
+                    My Name is {this.state.name}
+
+                </div>
+                < div className='wrapper' >
+                    Hello My Component from {this.state.name}
+                </div >
+                <div className='seconddiv'>
+                    <button onClick={() => this.handleClickButton()}>Click me</button>
+                </div>
+            </>
         )
     }
 
